d16a0798fb4609eb61823eb7e66f64a66d99a13d
moose2d <moose2d@DESKTOP-K8CQM7H.localdomain>
2023-01-17 17:49:24 -0700

    Menu button has basic functionality (hidden, expand upon click, etc)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const myImage = document.querySelector("img");
 
+/* --------- Image Display Example - changers on click ------------
+/*
 myImage.onclick = () => {
   const mySrc = myImage.getAttribute("src");
   if (mySrc === "images/heartNebula_2.jpg") {
@@ -8,21 +10,11 @@ myImage.onclick = () => {
     myImage.setAttribute("src", "images/heartNebula_2.jpg");
   }
 };
-
-/*
-let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
-
-function setUserName() {
-  const myName = prompt("Please enter your name.");
-  localStorage.setItem("name", myName);
-  myHeading.textContent = `Welcome to Space Sojourn LLC, ${myName}`;
-}
-
-if (!localStorage.getItem("name")) {
-  setUserName();
-} else {
-  const storedName = localStorage.getItem("name");
-  myHeading.textContent = `Welcome to Space Sojourn LLC, ${storedName}`;
-}
 */
+
+const navToggle = document.querySelector('.mobile-nav-toggle');
+const primaryNav = document.querySelector('.primary-navigation');
+
+navToggle.addEventListener("click", () => {
+  primaryNav.classList.toggle("opened");
+});
