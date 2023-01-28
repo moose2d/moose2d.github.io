1de3964bc198c00c556ada027ba10d2715fd257f
Jay Drobnick <jaydrobnick@Jays-MacBook-Pro-2.local>
2023-01-27 17:21:17 -0700

    Added sample code to the main.js including a game of RockPaperScissors

--- js/main.js
+++ js/main.js
@@ -12,13 +12,55 @@ myImage.onclick = () => {
 };
 */
 
-// BUTTON TOGGLES
-const navToggle = document.querySelector('.mobile-nav-toggle');
-const primaryNav = document.querySelector('.primary-navigation');
+const nextNewMoonDay = 23;
+const currentDay = 21;
 
-navToggle.addEventListener("click", () => {
-  primaryNav.hasAttribute("data-visible") 
-  ? navToggle.setAttribute("aria-expanded", false) 
-  : navToggle.setAttribute("aria-expanded", true);
-  primaryNav.toggleAttribute("data-visible");
-});
+function daysToNewMoon (currentDay, nextNewMoonDay) {
+  return nextNewMoonDay - currentDay;
+}
+
+console.log(`Days to new moon: ${daysToNewMoon(currentDay, nextNewMoonDay)}`);
+
+/* ---- EXAMPLE FUNCITONS ---- */
+
+// --- add7 ----
+/*
+function add7 (myNum) {
+  return myNum + 7;
+}
+
+console.log(`The result of the add7 function is ${add7(7)}`)
+*/
+
+// ======= ROCK PAPER SCISSORS =========
+/*
+let choices = ['Rock', 'Paper', 'Scissors'];
+let playerChoice;
+function getComputerChoice(choices) {
+  const random = Math.floor(Math.random() * choices.length);
+  computerChoice = choices[random].toLowerCase();
+  return computerChoice; 
+}
+
+function getPlayerChoice() {
+ playerChoice = prompt("Select Rock, Paper, or Scissors:" ).toLowerCase();
+ console.log(`Player chose ${playerChoice}`)
+}
+
+getPlayerChoice()
+
+function compareChoices (playerChoice, computerChoice) {
+  console.log(`Player choice is ${playerChoice} and computer choice is ${computerChoice}`)
+  if ((playerChoice == "Scissors" && computerChoice == "Paper")    ||
+      (playerChoice == "Rock"     && computerChoice == "Scissors") ||
+      (playerChoice == "Paper"    && computerChoice == "Rock")) {
+    console.log("Congratulations, you win!")
+  } else if (playerChoice == computerChoice) {
+    console.log("The match is a draw!")
+  } else {
+    console.log("Sorry, you lost to the computer!")
+  }
+}
+
+compareChoices(playerChoice, getComputerChoice(choices))
+*/
